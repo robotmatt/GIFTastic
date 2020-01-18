ca3ad05475861fc473a9e472bd32e5d423f90c58
Matt Martin <matt@enigma.local>
2020-01-17 21:31:03 -0600

    comments and readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # GIFTastic
+
+This is Homework #6 for the UT Austin Coding bootcamp. It is a fantastic search engine for GIFs that you can use in all of your messagin apps. 
+
+## Usage
+
+Click on a button, and the images will load based on the mood you're feeling. If you want more than the default, feel free to add a mood!
--- assets/js/code.js
+++ assets/js/code.js
@@ -1,3 +1,5 @@
+// Variable: topics
+// list of detaul topics to get GIFs for
 let topics = [
     "Cool",
     "Waiting",
@@ -9,28 +11,37 @@ let topics = [
     "Dancing"
 ]
 
+// Function: drawGif {array} 
+// Draws the GIFs on the page based on the GIFY API response
 function drawGif(element) {
     // log the element 
     console.log(element);
+
+    // Create the display elements
     var gifDiv = $("<div>").addClass("col-md-4 gif-div");
     var cardDiv = $("<div>").addClass("card mb-4 shadow-sm");
     var img = $("<img>");
-    img.attr("src", element.images.fixed_height_still.url);
-    img.addClass("gif-img");
-    img.attr("data-animate", element.images.fixed_height.url);
-    img.attr("data-still", element.images.fixed_height_still.url)
     var cardBody = $("<div>").addClass("card-body");
     var cardText = $("<p>").addClass("card-text");
     var btnContainer = $("<div>").addClass("d-flex justify-content-between align-items-center");
     var btnGroup = $("<div>").addClass("btn-group");
     var downloadBtn = $("<button>").addClass("btn btn-sm btn-outline-secondary download");
     
+    // set the download button properties
     downloadBtn.attr("onclick", "window.open('" + element.images.original.url + "')");
 
+    // set the image properties
+    img.attr("src", element.images.fixed_height_still.url);
+    img.addClass("gif-img");
+    img.attr("data-animate", element.images.fixed_height.url);
+    img.attr("data-still", element.images.fixed_height_still.url)
+
+    // set the text for the image
     cardText.text(element.title);
     cardText.append($("<p>").text("Rating: " + element.rating.toUpperCase()));
     downloadBtn.text("Download");
 
+    // add all the various divs to eachother
     gifDiv.append(cardDiv);
     cardDiv.append(img);
     cardDiv.append(cardBody);
@@ -38,9 +49,13 @@ function drawGif(element) {
     cardBody.append(btnContainer);
     btnContainer.append(btnGroup);
     btnGroup.append(downloadBtn);
+
+    // return the jQuery object to be displayed
     return gifDiv;
 }
 
+// Funcation: drawButtons {array}
+// Take in an array of topics and draw them on the page
 function drawButtons(topicList) {
     // Remove the current buttons
     $(".gif-btn").remove();
@@ -54,24 +69,20 @@ function drawButtons(topicList) {
         $("#buttons-here").append(button);
     });
 }
+
+// On click function of the gif-img class
+// Starts or stops the animation of the GIF
 $(document).on("click", ".gif-img", function () {
+    // if the source is still, then animate it
     if($(this).attr("src") === $(this).attr("data-still")){
         $(this).attr("src", $(this).attr("data-animate"));
-    } else{
+    } else { // otherwise stop it
         $(this).attr("src", $(this).attr("data-still"));
     }
-});
-$(document).on("click", ".animate", function () {
-    if($(this).attr("src") === $(this).attr("data-still")){
-        $(this).attr("src", $(this).attr("data-animate"));
-    } else{
-        $(this).attr("src", $(this).attr("data-still"));
-    }
-});
-$(document).on("click", ".pause", function () {
-    $(this).attr("src", $(this).attr("data-still"));
 });
 
+// On click function of the gif-btn class
+// Loads GIFs based on the button that was clicked
 $(document).on("click", ".gif-btn", function () {
     // Remove current GIFs
     $(".gif-div").remove();
@@ -80,6 +91,7 @@ $(document).on("click", ".gif-btn", function () {
     var apiKey = "T74dMsmdTp3yeLjLoyQNCm8jN3J2Szu3";
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&limit=12&offset=0&rating=PG-13&lang=en&q=" + topics[$(this).attr("data-index")];
     console.log(queryURL);
+
     // Sets up the jQuery ajax function to pull from the giphy API
     $.ajax({
         url: queryURL,
@@ -90,11 +102,14 @@ $(document).on("click", ".gif-btn", function () {
             console.log(response);
 
             response.data.forEach(function (element, index) {
+                // Draw the gifs on the page
                 $("#gif-display").append(drawGif(element));
             });
         });
 });
 
+// On click function to add a new button
+// Adds a button to the page based on the input
 $("#new-phrase-btn").on("click", function(){
     event.preventDefault();
     topics.push($("#new-phrase-input").val().trim());
@@ -104,5 +119,6 @@ $("#new-phrase-btn").on("click", function(){
 
 
 $(document).ready(function () {
+    // Draw the initial set of topics when the page loads
     drawButtons(topics);
 });
